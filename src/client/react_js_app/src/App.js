77954b18ca2ff5957c330cc0512ef11910a2626f
Fabian-Schneider01 <xf.schneider01@gmail.com>
2023-03-10 13:29:32 +0100

    feat: speech to text

--- src/client/react_js_app/src/App.js
+++ src/client/react_js_app/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import logo from './cAPItan_Logo.jpg';
 
-import React, { useState } from 'react';
+import React, { useState, useSyncExternalStore } from 'react';
+import SpeechRecognition, {
+  useSpeechRecognition
+} from 'react-speech-recognition';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
@@ -9,6 +12,29 @@ function App() {
   const handleChange = (event) => {
     setInputValue(event.target.value);
   };
+
+  const { transcript, resetTranscript } = useSpeechRecognition({
+    continuous: true
+  });
+ 
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    alert("This Browser doesn't support Speech-to-Text");
+    return null;
+  }
+
+  //sending text to flask application
+  const sendTranscript = () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ transcript: transcript })
+    };
+    fetch('/submit_transcript', requestOptions)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  };
+
 
   return (
     <div className="App">
@@ -23,7 +49,20 @@ function App() {
         </div>
        
       </div>
-     
+      <div className="record-container">
+        <div >
+          <p></p>
+          <textarea value={transcript} className="converted-speech">
+          
+          </textarea>
+        </div>
+        <div>
+          <button onClick={SpeechRecognition.startListening}>Record</button>
+          <button onClick={sendTranscript}>Submit</button>
+        </div>
+      </div>
+      <div>
+    </div>
     </div>
   );
 }
