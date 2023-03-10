6d0338032f51d21e7e56b3905cb4b7146cf35d8c
Max Kiefer <81292206+Maxkie1@users.noreply.github.com>
2023-03-10 12:18:40 +0100

    refactor: add get params

--- src/services/weather_service/app.py
+++ src/services/weather_service/app.py
@@ -24,12 +24,22 @@ def get_weather():
         The weather information for the next day for the given location.
     """
     
-    api_key = '10245a3d06eb6a826ddc1bfa3d943829'
     lat = 51.5074
-    lon = 0.1278 
-    url = f'https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={1}&appid={api_key}'
+    lon = 0.1278
+    cnt = 1
+    units = 'metric'
+    api_key = '10245a3d06eb6a826ddc1bfa3d943829'
 
-    response = requests.get(url)
+    url = f'https://api.openweathermap.org/data/2.5/forecast/daily'
+    params = {
+        'lat': lat,
+        'lon': lon,
+        'cnt': cnt,
+        'units': units,
+        'appid': api_key
+    }
+
+    response = requests.get(url, params=params)
     if response.status_code != 200:
         jsonify({'error': 'Error getting weather information'}), 500
 
