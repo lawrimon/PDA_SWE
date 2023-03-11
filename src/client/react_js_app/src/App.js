69461e668f3a3b811ad00fca592e0b16ac94e0ab
Fabian-Schneider01 <xf.schneider01@gmail.com>
2023-03-11 18:03:14 +0100

    feat: text to speech

--- src/client/react_js_app/src/App.js
+++ src/client/react_js_app/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import logo from './cAPItan_Logo.jpg';
-
 import React, { useState, useSyncExternalStore } from 'react';
 import SpeechRecognition, {
   useSpeechRecognition
@@ -8,11 +7,27 @@ import SpeechRecognition, {
 
 function App() {
   const [inputValue, setInputValue] = useState('');
+  const [textToSpeak, setTextToSpeak] = useState('');
+  const { SpeechSynthesisUtterance, speechSynthesis } = window;
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
   };
 
+  const handleTextChange = (event) => {
+    setTextToSpeak(event.target.value);
+  };
+
+  const handleSpeak = () => {
+    if (speechSynthesis.speaking) {
+      return; 
+    }
+    const utterance = new SpeechSynthesisUtterance(transcript);
+    utterance.rate = 0.9;
+    utterance.pitch = 1;
+    speechSynthesis.speak(utterance);
+  };
+  
   const { transcript, resetTranscript } = useSpeechRecognition({
     continuous: true
   });
@@ -22,7 +37,6 @@ function App() {
     return null;
   }
 
-  //sending text to flask application
   const sendTranscript = () => {
     const requestOptions = {
       method: 'POST',
@@ -35,7 +49,6 @@ function App() {
       .catch(error => console.error(error));
   };
 
-
   return (
     <div className="App">
       <div style={{marginTop:"5%"}}>
@@ -47,22 +60,17 @@ function App() {
         <div>
         <button type="button">Search</button>
         </div>
-       
       </div>
       <div className="record-container">
         <div >
-          <p></p>
-          <textarea value={transcript} className="converted-speech">
-          
-          </textarea>
+          <textarea value={transcript} onChange={handleTextChange} className="converted-speech"></textarea>
         </div>
         <div>
           <button onClick={SpeechRecognition.startListening}>Record</button>
+          <button onClick={handleSpeak}>Speak</button>
           <button onClick={sendTranscript}>Submit</button>
         </div>
       </div>
-      <div>
-    </div>
     </div>
   );
 }
