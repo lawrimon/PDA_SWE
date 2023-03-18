6cf1f6ebbd0b72406e73f5ce7b601b723343511d
gregor434 <gregor434@users.noreply.github.com>
2023-03-18 13:18:44 +0000

    refactor: auto-format python code

--- src/database/test/test_database.py
+++ src/database/test/test_database.py
@@ -3,29 +3,34 @@ from fakeredis import FakeStrictRedis
 from database.app import app
 import json
 
+
 @pytest.fixture
 def client():
-
     """Create a test client for the app."""
 
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.test_client() as client:
         with app.app_context():
             yield client
+
 
 @pytest.fixture
 def fake_redis(monkeypatch):
     """Replace the redis_store with a fake redis instance."""
     redis = FakeStrictRedis()
-    monkeypatch.setattr('database.app.redis_store', redis)
+    monkeypatch.setattr("database.app.redis_store", redis)
     return redis
 
-@pytest.mark.parametrize("user_id, preferences", [
-    ("user1", {"language": "en", "football_club": "Real Madrid"}),
-    ("user2", {"username": "johndoe", "password": "password123"}),
-])
+
+@pytest.mark.parametrize(
+    "user_id, preferences",
+    [
+        ("user1", {"language": "en", "football_club": "Real Madrid"}),
+        ("user2", {"username": "johndoe", "password": "password123"}),
+    ],
+)
 def test_add_user(client, fake_redis, user_id, preferences):
-    response = client.post('/users', json={"user_id": user_id, **preferences})
+    response = client.post("/users", json={"user_id": user_id, **preferences})
     assert response.status_code == 200
     assert json.loads(response.data) == {"status": "success, user added"}
 
@@ -34,18 +39,36 @@ def test_add_user(client, fake_redis, user_id, preferences):
         assert fake_redis.hget(user_id, key).decode() == str(value)
 
 
-@pytest.mark.parametrize("user_id, initial_preferences, updated_preferences, expected_preferences", [
-    ("user1", {"language": "en", "football_club": "Real Madrid"}, {"language": "es", "football_club": "Barcelona"},
-     {"language": "es", "football_club": "Barcelona"}),
-    ("user2", {"username": "johndoe", "password": "password123"}, {"password": "newpassword"},
-     {"username": "johndoe", "password": "newpassword"}),
-])
-def test_update_user(client, fake_redis, user_id, initial_preferences, updated_preferences, expected_preferences):
+@pytest.mark.parametrize(
+    "user_id, initial_preferences, updated_preferences, expected_preferences",
+    [
+        (
+            "user1",
+            {"language": "en", "football_club": "Real Madrid"},
+            {"language": "es", "football_club": "Barcelona"},
+            {"language": "es", "football_club": "Barcelona"},
+        ),
+        (
+            "user2",
+            {"username": "johndoe", "password": "password123"},
+            {"password": "newpassword"},
+            {"username": "johndoe", "password": "newpassword"},
+        ),
+    ],
+)
+def test_update_user(
+    client,
+    fake_redis,
+    user_id,
+    initial_preferences,
+    updated_preferences,
+    expected_preferences,
+):
     # add user to Redis with initial preferences
     for key, value in initial_preferences.items():
         fake_redis.hset(user_id, key, value)
 
-    response = client.put(f'/users/{user_id}', json=updated_preferences)
+    response = client.put(f"/users/{user_id}", json=updated_preferences)
     assert response.status_code == 200
     assert json.loads(response.data) == {"status": "success, user updated"}
 
@@ -54,16 +77,19 @@ def test_update_user(client, fake_redis, user_id, initial_preferences, updated_p
         assert fake_redis.hget(user_id, key).decode() == str(value)
 
 
-@pytest.mark.parametrize("user_id, preferences", [
-    ("user1", {"language": "en", "football_club": "Real Madrid"}),
-    ("user2", {"username": "johndoe", "password": "password123"}),
-])
+@pytest.mark.parametrize(
+    "user_id, preferences",
+    [
+        ("user1", {"language": "en", "football_club": "Real Madrid"}),
+        ("user2", {"username": "johndoe", "password": "password123"}),
+    ],
+)
 def test_get_user(client, fake_redis, user_id, preferences):
     # add user to Redis with preferences
     for key, value in preferences.items():
         fake_redis.hset(user_id, key, value)
 
-    response = client.get(f'/users/{user_id}')
+    response = client.get(f"/users/{user_id}")
     assert response.status_code == 200
     assert json.loads(response.data) == preferences
 
@@ -73,9 +99,9 @@ def test_delete_user(client, fake_redis, user_id):
     # add user to Redis
     fake_redis.hset(user_id, "language", "en")
 
-    response = client.delete(f'/users/{user_id}')
+    response = client.delete(f"/users/{user_id}")
     assert response.status_code == 200
-    assert json.loads(response.data) == {'status': 'success, user deleted'}
+    assert json.loads(response.data) == {"status": "success, user deleted"}
 
     # check if the user was deleted from Redis
     assert not fake_redis.exists(user_id)
