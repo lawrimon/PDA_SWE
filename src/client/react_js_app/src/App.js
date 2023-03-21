fae07d9094b52c6e66080ffd9e4321f45d89855d
Max Kiefer <81292206+Maxkie1@users.noreply.github.com>
2023-03-21 12:51:17 +0100

    Revert "Feature/96"

--- src/client/react_js_app/src/App.js
+++ src/client/react_js_app/src/App.js
@@ -4,19 +4,12 @@ import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Settings } from "./pages/Settings"
 import React, { useState } from 'react';
-import LoginPage from './pages/Login';
-import RegisterPage from './pages/Register';
-import PreferencesPage from './pages/Preferences';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/settings" element={<Settings />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path ="/register" element={<RegisterPage/>}/>
-      <Route path ="/preferences" element={<PreferencesPage/>}/>
-
     </Routes>
   )
 }
