3e7e13084e48ed98adea03f23bf01515e0bd3c5d
Fabian-Schneider01 <xf.schneider01@gmail.com>
2023-03-30 17:36:53 +0200

    feat: basic webhook

--- src/services/language_service/app.py
+++ src/services/language_service/app.py
@@ -1,63 +1,33 @@
-"""This application is the stock market service.
-
-The maps service provides an endpoint to get the latest quote of a list of symbols.
-It also provides an endpoint to get the latest news of a list of symbols.
-The functionality is based on the Alpaca API.
-"""
-
-from flask import Flask, jsonify, request
-import requests
 import os
-import datetime
-import spacy
-import json
+import uuid
+from flask import Flask, request
+from google.cloud import dialogflow_v2 as dialogflow
+from google.api_core.exceptions import InvalidArgument
 
-# nlp = spacy.load("de_core_news_md")  # Laden des mittelgroßen Modells von spaCy
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "capitan-382017-1431f46e0617.json"
 
-# # Definieren der zu vergleichenden Sätze
-# sentence1 = nlp("Zeig mir was es in der Nähe für Events gibt")
-# sentence2 = nlp("Wo gibt es Events")
+session_id = str(uuid.uuid4())
 
-# # Vergleichen der Ähnlichkeit der beiden Sätze
-# similarity = sentence1.similarity(sentence2)
+def detect_intent_from_text(text):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path("capitan-382017", session_id)
 
-# # Ausgabe des Ergebnisses
-# if similarity >= 0.7:
-#     print("Die Sätze sind ähnlich genug.")
-# else:
-#     print("Die Sätze sind nicht ähnlich genug.")
+    text_input = dialogflow.TextInput(text=text, language_code="en")
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+    except InvalidArgument:
+        raise
+
+    return response.query_result.intent.display_name
+
 app = Flask(__name__)
-with open('prompts.json', 'r') as f:
-    prompts = json.load(f)
 
-@app.route("/submit_transcript", methods=['POST'])
-def load_prompts():
-    """Quotes endpoint.
-
-    This endpoint provides the latest quotes of a list of symbols.
-
-    Args:
-        symbols: A list of symbols (e.g. "IBM,MSFT,GOOG").
-
-    Returns:
-        A list of the latest quotes of the symbols.
-    """
-    user_input = request.json['input']
-
-    # Check if user input matches any prompt
-    for prompt in prompts['topic1']:
-        if user_input == prompt:
-            return jsonify({'response': 'User input matches prompt.'})
-
-    check_intent(user_input)
-    return jsonify({'response': 'User input does not match any prompt.'})
-    print("this is a test")
-
-def load_prompts():
-
-def compare_prompts():
-
-def check_intent():
+@app.route('/')
+def detect_intent():
+    intent = detect_intent_from_text("Tell me about any noteworthy news that happened recently")
+    return f'The intent is {intent}'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=8002)
---
+++ src/services/language_service/tests/test_intent_detection.py
@@ -0,0 +1,7 @@
+import pytest
+from maps_service.app import app, is_valid_coordinates_string
+
+
+@pytest.fixture
+def test():
+    return 0
