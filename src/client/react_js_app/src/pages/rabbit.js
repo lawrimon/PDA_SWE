7dfc1063323c38404912c01fb150a6499202e1e3
Max Kiefer <81292206+Maxkie1@users.noreply.github.com>
2023-04-04 18:04:29 +0200

    fix: make main work again

--- src/client/react_js_app/src/pages/rabbit.js
+++ src/client/react_js_app/src/pages/rabbit.js
@@ -1,77 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import Connection from 'rabbitmq-client';
 
 const RabbitMqConsumer = () => {
-  const [messages, setMessages] = useState([]);
-  const [connection, setConnection] = useState(null);
-
-  useEffect(() => {
-    const rabbit = new Connection({
-      url: 'amqp://guest:guest@localhost:5672',
-      retryLow: 1000,
-      retryHigh: 30000,
-    });
-
-    rabbit.on('error', (err) => {
-      console.error(err);
-    });
-
-    rabbit.on('connection', () => {
-      console.log('The connection is successfully (re)established');
-    });
-
-    async function consumeMessages() {
-      const ch = await rabbit.acquire();
-
-      await ch.queueDeclare({ queue: 'notifications', exclusive: true });
-
-      await ch.basicConsume({ queue: 'notifications' }, (msg) => {
-        setMessages((prevMessages) => [...prevMessages, msg.content.toString()]);
-        ch.basicAck({ deliveryTag: msg.deliveryTag });
-      });
-    }
-
-    rabbit
-      .connect()
-      .then(() => {
-        console.log('Connected to RabbitMQ');
-        setConnection(rabbit);
-        consumeMessages();
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-
-    return () => {
-      if (connection) {
-        connection.close();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log(messages);
-  }, [messages]);
-
-  return (
-    <div>
-      <h1>Incoming Messages:</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Message</th>
-          </tr>
-        </thead>
-        <tbody>
-          {messages.map((message, index) => (
-            <tr key={index}>
-              <td>{message}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+  
 };
 
 export default RabbitMqConsumer;
