0c6fd9fdf752477f05b1a172d7076196b9136f0d
Fabian-Schneider01 <xf.schneider01@gmail.com>
2023-04-12 14:17:21 +0200

    feat: basic calendar

--- src/services/calendar_service/app.py
+++ src/services/calendar_service/app.py
@@ -11,5 +11,68 @@ Typical endpoint usage:
 
 """
 
+from __future__ import print_function
+
+import datetime
+import os.path
+
+from flask import Flask, jsonify
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+app = Flask(__name__)
+
+@app.route("/events")
+def get_calendar_events():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        events_result = service.events().list(calendarId='primary', timeMin=now, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            return jsonify({"error": "No upcoming events found."}), 404
+
+        # Convert the events to a list of dictionaries
+        events_list = []
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            events_list.append({"start": start, "summary": event['summary']})
+
+        return jsonify(events_list)
+
+    except HttpError as error:
+        return jsonify({"error": "An error occurred: %s" % error}), 500
+
+if __name__ == '__main__':
+    app.run()
+
 
 
