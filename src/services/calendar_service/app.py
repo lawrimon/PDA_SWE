125e41c21c0c2f8c81c82b090517d5423379aa26
Fabian-Schneider01 <xf.schneider01@gmail.com>
2023-04-12 16:15:16 +0200

    feat: add calendar appointment

--- src/services/calendar_service/app.py
+++ src/services/calendar_service/app.py
@@ -24,29 +24,13 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 app = Flask(__name__)
 
-@app.route("/events")
-def get_calendar_events():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+@app.route("/calendar/getappointment")
+def get_calendar_appointments():
+    creds = get_creds()
 
     try:
         service = build('calendar', 'v3', credentials=creds)
@@ -70,6 +54,62 @@ def get_calendar_events():
 
     except HttpError as error:
         return jsonify({"error": "An error occurred: %s" % error}), 500
+
+@app.route("/calendar/addappointment")
+def add_calendar_appointments():
+    creds = get_creds()
+    summary = "test event"
+    start_time = "2023-04-13T16:00:00+02:00"
+    end_time = "2023-04-13T17:00:00+02:00"
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        event = {
+            'summary': summary,
+            'start': {
+                'dateTime': start_time,
+                'timeZone': 'UTC',
+            },
+            'end': {
+                'dateTime': end_time,
+                'timeZone': 'UTC',
+            },
+        }
+
+        event = service.events().insert(calendarId='primary', body=event).execute()
+
+        return jsonify({"success": "Event added successfully."}), 200
+
+    except HttpError as error:
+        return jsonify({"error": "An error occurred: %s" % error}), 500
+
+
+
+@app.route("/calendar/deleteappointment")
+def delete_calendar_appointments():
+    return 0
+
+def get_creds():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    
+    return creds
 
 if __name__ == '__main__':
     app.run()
