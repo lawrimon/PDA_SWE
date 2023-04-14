ad650144f9dc4b357c0bcfae3e925fb5d63031ff
lawrimon <81292176+lawrimon@users.noreply.github.com>
2023-04-14 14:07:59 +0200

    try: implementing scuttlebutt rabitjs

--- src/use_cases/use_case_gateway/use_casessss/app.py
+++ src/use_cases/use_case_gateway/use_casessss/app.py
@@ -1,29 +1,16 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from flask import Flask, jsonify
+from flask import Flask
 import json
 import requests
 import pika
 from datetime import datetime, timedelta
-
 
 app = Flask(__name__)
 
 RABBITMQ_HOST = "rabbitmq"
 
 # Mock users
-#users = ["user1", "user2", "user3"]
-
-def get_all_user():
-    url = "http://db:5000/allUsers"
-
-    response = requests.get(url)
-    if response.status_code != 200:
-        return jsonify({"error": "Error getting books information"}), 500
-
-    users = response.json()
-
-    return users
-
+users = ["user1", "user2", "user3"]
 
 # to get all users in production have a look at the "/allusers" Endpoint in database app.py
 # something like: allUsers = requests.get("http://db/allUsers")
@@ -66,19 +53,6 @@ def get_nearby_events(location):
     else:
         return None
 
-def get_scuttlebutt(user):
-    """
-    This function retrieves the scuttlebutt usecase.
-    """
-    url = "http://scuttlebutt:5000/scuttlebutt?user="+ user
-
-    response = requests.get(url)
-    if response.status_code != 200:
-        return jsonify({"error": "Error getting books information"}), 500
-
-    message = response.json()
-
-    return message
 
 def check_appointment_interference(appointments, event):
     """
@@ -121,34 +95,31 @@ def notify_users():
     channel = connection.channel()
     channel.exchange_declare(exchange="notifications", exchange_type="direct")
 
-    users = get_all_user()
-
     for user_id in users:
         channel.queue_declare(queue=user_id)
         channel.queue_bind(exchange="notifications", queue=user_id, routing_key=user_id)
-        
-        '''
-        Todo: call the scuttlebutt function and check for problems 
-        -> Checking for problems should happen using the calendar service!
-        '''
-        message = get_scuttlebutt(user_id)
 
-        if(message):
-            # convert the event object to a string before publishing
-            event_str = json.dumps(message)
-            # publish the event to the queue
-            channel.basic_publish(
-                exchange="notifications", routing_key=user_id, body=event_str
-            )
+        location = get_user_location(user_id)
+        appointments = get_user_appointments(user_id)
+        events = get_nearby_events(location)
 
+        if events:
+            for event in events["data"]:
+                if not check_appointment_interference(appointments["data"], event):
+                    # convert the event object to a string before publishing
+                    event_str = json.dumps(event)
+                    # publish the event to the queue
+                    #channel.basic_publish(
+                    #    exchange="notifications", routing_key=user_id, body=event_str
+                    #)
     channel.close()
     connection.close()
 
 
 # publish every 7 seconds
-scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(func=notify_users, trigger="interval", seconds=30)
-scheduler.start()
+#scheduler = BackgroundScheduler(daemon=True)
+#scheduler.add_job(func=notify_users, trigger="interval", seconds=7)
+#scheduler.start()
 
 if __name__ == "__main__":
     app.run(debug=True)
