c504b4278b45aab616ac4814be37ef2ac233c1ad
Fabian-Schneider01 <Fabian-Schneider01@users.noreply.github.com>
2023-04-18 08:29:12 +0000

    refactor: auto-format python code

--- src/services/language_service/app.py
+++ src/services/language_service/app.py
@@ -10,18 +10,21 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "capitan-382017-1431f46e0617.json
 
 app = Flask(__name__)
 
+
 def create_session_id():
     return str(uuid.uuid4())
 
-def detect_intent_from_text(project_id, session_id, text):
 
+def detect_intent_from_text(project_id, session_id, text):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
     text_input = dialogflow.TextInput(text=text, language_code="en")
     query_input = dialogflow.QueryInput(text=text_input)
 
     try:
-        response = session_client.detect_intent(session=session, query_input=query_input)
+        response = session_client.detect_intent(
+            session=session, query_input=query_input
+        )
     except InvalidArgument:
         raise
 
@@ -29,16 +32,19 @@ def detect_intent_from_text(project_id, session_id, text):
 
     return intent
 
-@app.route('/get_intent')
+
+@app.route("/get_intent")
 def submit_transcript():
     transcript = request.args.get("transcript")
     try:
-        intent = detect_intent_from_text("capitan-382017", create_session_id(), transcript)
+        intent = detect_intent_from_text(
+            "capitan-382017", create_session_id(), transcript
+        )
     except ValueError as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-    return jsonify({'intent': intent})
+    return jsonify({"intent": intent})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=8003)
-
