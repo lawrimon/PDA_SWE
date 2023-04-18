9bfdac0470fa5aadc98ea09ae648b2771de53e1d
Max Kiefer <81292206+Maxkie1@users.noreply.github.com>
2023-04-18 15:24:52 +0200

    fix: fix error string

--- src/use_cases/lookoutduty/app.py
+++ src/use_cases/lookoutduty/app.py
@@ -111,7 +111,7 @@ def get_sports(club_ids, clubs):
         url = f"http://sports:5000/football/fixture"
         response = requests.get(url, params)
         if response.status_code != 200:
-            return "No sports fixtures found for the provided team."
+            return "No sports fixtures found for the provided team. "
             # jsonify({"error": "Error getting weather information"}), 500
 
         data = response.json()
